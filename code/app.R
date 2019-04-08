db31cb141c09d46302997e9ad97626c8c7b7a052
Alessandro Samuel-Rosa <alessandrosamuelrosa@gmail.com>
2019-04-08 14:31:12 -0300

    define versão atual como 0.2.0

--- code/app.R
+++ code/app.R
@@ -1,5 +1,5 @@
 # Title: Repositório Brasileiro Livre para Dados Abertos do Solo - aplicação Shiny
-# Version: 2.0.0
+# Version: 0.2.0
 # Date: 2019-03-08
 # Authors: Matheus Ferreira Ramos (), Alessandro Samuel-Rosa (alessandrorosa@utfpr.edu.br)
 # License: GPL (>= 2)
