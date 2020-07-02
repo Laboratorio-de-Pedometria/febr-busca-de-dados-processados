5e651dfbf71cbeea6f069b273978e8d724061e0e
Alessandro Samuel-Rosa <alessandrosamuelrosa@gmail.com>
2020-07-02 19:01:47 -0300

    new app name

--- deployapp.R
+++ deployapp.R
@@ -2,4 +2,4 @@ rsconnect::deployApp(
   appDir = 'app', 
   appFiles = c("app.R", "www"),
   appName = "FEBR",
-  appTitle = "FEBR | Busca de Dados Padronizados")
+  appTitle = "FEBR | Dados Processados")
